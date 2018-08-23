cf4e3fcc15dfae40795254499641f8a777e74da8
ben-vandelaar <benvandelaar@gmail.com>
2018-08-23 19:12:53 +1200

    deploying

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
+
 module.exports = {
   getWalks
 }
@@ -10,6 +11,7 @@ function getWalks (testConn) {
   const conn = testConn || connection
   return conn('walks').select()
 }
+
 
 // function getUser (id, testConn) {
 //   const conn = testConn || connection
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,4 @@
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    },
-    useNullAsDefault: true
-  },
-
   test: {
     client: 'sqlite3',
     connection: {
