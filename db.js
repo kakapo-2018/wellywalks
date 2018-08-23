3f5564c3b2ea839bf79777a0a745bd0a457123b2
leslie-alldridge <leslie-alldridge>
2018-08-23 17:05:12 +1200

    initial commit

---
+++ db.js
@@ -0,0 +1,18 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  getUser: getUser,
+  getUsers: getUsers
+}
+
+function getUsers (testConn) {
+  const conn = testConn || connection
+  return conn('users').select()
+}
+
+function getUser (id, testConn) {
+  const conn = testConn || connection
+  return conn('users').where('id', id).first()
+}
