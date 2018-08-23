7e5dfa20d54354ef2209c14d67f71df3e232ebb5
student <leslie-alldridge>
2018-08-24 10:57:14 +1200

    profiles db works

--- seeds/profiles.js
+++ seeds/profiles.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('profiles').insert([
-        {id: 1, profile_id: '123', name: 'Clark Kent', bio: 'Born in Kryton, raised in Smallville, Kansas', image: 'url1' },
-        {id: 2, profile_id: '345', name: 'Bruce Wayne', bio: 'Bruce Wayne swore an oath to rid the city of the evil that had taken his parents lives. He spent his youth traveling the world, training himself to intellectual and physical perfection and learning a variety of crime-fighting skills, including chemistry, criminology, forensics, martial arts, gymnastics, disguise, and escape artistry.', image: 'url2' },
-        {id: 3, profile_id: '678', name: 'Hal Jordan', bio: 'Hal Jordan is a fighter pilot, a member and occasionally leader of an intergalactic police force called the Green Lantern Corps, as well as a founding member of the Justice League.', image: 'url3' },
+        {id: 1, name: 'Clark Kent', level: 3,  bio: 'Born in Kryton, raised in Smallville, Kansas', image: 'url1' },
+        {id: 2, name: 'Bruce Wayne', level: 3, bio: 'Bruce Wayne swore an oath to rid the city of the evil that had taken his parents lives. He spent his youth traveling the world, training himself to intellectual and physical perfection and learning a variety of crime-fighting skills, including chemistry, criminology, forensics, martial arts, gymnastics, disguise, and escape artistry.', image: 'url2' },
+        {id: 3, name: 'Hal Jordan', level: 3, bio: 'Hal Jordan is a fighter pilot, a member and occasionally leader of an intergalactic police force called the Green Lantern Corps, as well as a founding member of the Justice League.', image: 'url3' },
       ]);
     });
 };
